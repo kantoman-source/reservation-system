3ded958c5fd88632dd779ec8b107739458bdbd0a
kantoman-source <kantomanpc@gmail.com>
2026-09-13 18:03:47 +0900

    debugコード

--- api/reserve.ts
+++ api/reserve.ts
@@ -26,14 +26,27 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   // GET /api/reserve?month=2026-10
   if (method === 'GET' && query.month) {
-    const [year, month] = (query.month as string).split('-').map(Number);
+    try {
+      const [year, month] = (query.month as string).split('-').map(Number);
 
-    const result = await db.query(
-      "SELECT date, time FROM reservations WHERE EXTRACT(YEAR FROM date) = $1 AND EXTRACT(MONTH FROM date) = $2",
-      [year, month]
-    );
+      const result = await db.query(
+        "SELECT date, time FROM reservations WHERE EXTRACT(YEAR FROM date) = $1 AND EXTRACT(MONTH FROM date) = $2",
+        [year, month]);
+        return res.status(200).json(result.rows);
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({
+        error: String(e)
+        });
+    }
+    // const [year, month] = (query.month as string).split('-').map(Number);
 
-    return res.status(200).json(result.rows);
+    // const result = await db.query(
+    //   "SELECT date, time FROM reservations WHERE EXTRACT(YEAR FROM date) = $1 AND EXTRACT(MONTH FROM date) = $2",
+    //   [year, month]
+    // );
+
+    // return res.status(200).json(result.rows);
   }
 
   // GET /api/reserve?date=2026-10-01
